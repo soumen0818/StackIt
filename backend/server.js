c1fc0a8ab0c001db9de218576691d467dd931fce
soumen0818 <sdas721444@gmail.com>
2025-07-12 16:58:11 +0530

    Final Push in the project

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -22,7 +23,7 @@ app.use("/api/notifications", notificationRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb://localhost:27017/stackit", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
